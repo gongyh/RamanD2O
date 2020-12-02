9f1a2829dea4f8f6d23a86b0631fde6038de6846
gongyh <gongyh@qibebt.ac.cn>
2020-12-02 09:55:43 +0800

    implement logic to store spectra data to database

--- inst/shinyapp/server/database.R
+++ inst/shinyapp/server/database.R
@@ -1,11 +1,67 @@
 # connect mongodb on click of button
 observeEvent(input$connectdb, {
-  withBusyIndicatorServer("connectdb", {
-    tryCatch({
+  tryCatch(
+    {
       mongo_connection$obj <- mongo(collection = input$collection, db = input$db, url = input$url)
-    }, error = function(e) {
+    },
+    error = function(e) {
       shinyalert("Oops!", e$message, type = "error")
-    })
+    },
+    finally = {
+      output$db_message <- renderText({
+        paste0("MongoDB is connected at ", Sys.time())
+      })
+      shinyjs::disable("connectdb")
+    }
+  )
+})
 
+observeEvent(
+  {
+    input$collection
+    input$db
+    input$url
+  },
+  {
+    shinyjs::enable("connectdb")
+  }
+)
+
+output$hs_select_for_database <- renderUI({
+  hs_all <- names(hs$val)
+  selected <- NULL
+  if ("normalized" %in% hs_all) {
+    selected <- "normalized"
+  } else if ("raw" %in% hs_all) {
+    selected <- "raw"
+  }
+  selectInput("hs_selector_for_database", "Choose target", choices = hs_all, selected = selected)
+})
+
+# save spectra to database on click of button
+observeEvent(input$savedb, {
+  req(mongo_connection$obj)
+  withBusyIndicatorServer("savedb", {
+    if (input$hs_selector_for_database == "") {
+      shinyalert("Oops!", "Please first load your spectra data.", type = "error")
+      return()
+    } else {
+      hs_cur <- hs$val[[input$hs_selector_for_database]]
+      hs_df <- as.wide.df(hs_cur)
+      hs_df$dtype <- input$hs_selector_for_database
+      tryCatch(
+        {
+          mongo_connection$obj$insert(hs_df)
+        },
+        error = function(e) {
+          shinyalert("Oops!", e$message, type = "error")
+        },
+        finally = {
+          output$db_message <- renderText({
+            paste0("Data loaded into database at ", Sys.time())
+          })
+        }
+      )
+    }
   })
 })
--- inst/shinyapp/ui/database.R
+++ inst/shinyapp/ui/database.R
@@ -12,15 +12,21 @@ tabItem(
           column(2, textInput("collection", "Collection", value = "test")),
           column(2, textInput("db", "db", value = "test")),
           column(5, textInput("url", "url", value = "mongodb://localhost")),
-          column(3, withBusyIndicatorUI(actionButton("connectdb", "Connect", class = "btn-success")), class = "top25")
+          column(3, actionButton("connectdb", "Connect", class = "btn-success"), class = "top25")
+        ),
+        verbatimTextOutput("db_message", placeholder = T),
+        hr(),
+        fluidRow(
+          column(6, uiOutput("hs_select_for_database")),
+          column(6, withBusyIndicatorUI(actionButton("savedb", "Load to database", class = "btn-success")), class = "top25")
         )
       )
     ),
     box(
       title = "Notes", status = "info", solidHeader = TRUE, collapsible = FALSE,
       column(
-        12, p("The URL is in Connection String URI Format, the standard URI connection scheme has the form:",
-            p("mongodb://[username:password@]host1[:port1][,...hostN[:portN]][/[defaultauthdb][?options]]"))
+        12, p("The URL is in Connection String URI Format, the standard URI connection scheme has the form:"),
+        p("mongodb://[username:password@]host1[:port1][,...hostN[:portN]][/[defaultauthdb][?options]]")
       )
     )
   )
