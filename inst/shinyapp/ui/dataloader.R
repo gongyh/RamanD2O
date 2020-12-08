1a4f3250a08e0f18947fa1d66b5b663626254387
gongyh <gongyh@qibebt.ac.cn>
2020-12-08 21:20:36 +0800

    minor improve

--- inst/shinyapp/ui/dataloader.R
+++ inst/shinyapp/ui/dataloader.R
@@ -7,6 +7,9 @@ tabItem(
     box(
       title = "Uploader", status = "info", solidHeader = TRUE, collapsible = FALSE,
       br(),
+      p("If have imported your data from external database, then no need to reload here.", style = "color:red;"),
+      withBusyIndicatorUI(actionLink("load_demo", "Quick start, click to load demo data", class = "primary")),
+      br(),
       fluidRow(
         column(6, fileInput("scrs_file", "1. Upload SCRS Zip file", accept = ".zip", placeholder = "SCRS.zip")),
         column(6, withBusyIndicatorUI(actionButton("unzip", "Load SCRS", class = "btn-success")), class = "top25")
@@ -15,11 +18,8 @@ tabItem(
         column(6, fileInput("meta_file", "2. Upload Metadata", accept = ".tsv", placeholder = "meta.tsv")),
         column(6, withBusyIndicatorUI(actionButton("load_meta", "Load metadata", class = "btn-success")), class = "top25")
       ),
-      h4("Notes:"),
+      h5("Notes:"),
       p("The first column of the metadata table should be ID_Cell, which contains spectra file names without suffix."),
-      br(),
-      p("If have imported your data from external database, then no need to reload here.", style = "color:red;"),
-      withBusyIndicatorUI(actionLink("load_demo", "Quick start, click to load demo data", class = "primary")),
       br()
     ),
     box(
