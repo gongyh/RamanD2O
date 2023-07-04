aa6f8453b8fb7d72113fbd4b9004176e0805d96a
zhangjiayouc <zhangjs@qibebt.ac.cn>
2023-07-04 10:59:30 +0800

    update select multiple regions for ml_prepare

--- inst/shinyapp/server/ml_prepare.R
+++ inst/shinyapp/server/ml_prepare.R
@@ -11,13 +11,38 @@ output$hs_select_for_ml_prepare <- renderUI({
   selectInput("hs_selector_for_ml_prepare", "Choose target", choices = hs_all, selected = selected)
 })
 
-# convert prepare_trim_range and prepare_trim_min/max
-observeEvent(c(input$ptrim_min, input$ptrim_max), {
-  updateSliderInput(session, "ptrim_range", value = c(input$ptrim_min, input$ptrim_max))
+ml_trim_num <- reactiveVal(1)
+
+output$ml_trim_multi <- renderUI({
+  inputs <- lapply(1:ml_trim_num(), function(i) {
+    fluidRow(
+      column(2, numericInput(inputId = paste0("ml_trim_min", i), "Min", min = 0, max = 4000, step = 1, value = 400, width = "100%")),
+      column(8,
+             sliderInput(inputId = paste0("ml_trim_range", i), label = paste0("Selecting Wavelength Ranges: ", i), 
+                         min = 0, max = 4000, value = c(400, 3400), step = 1, dragRange = F, width = "100%")),
+      column(2, numericInput(inputId = paste0("ml_trim_max", i), "Max", min = 0, max = 4000, step = 1, value = 3400, width = "100%"))
+    )
+  })
+  # tagList(inputs)
 })
-observeEvent(c(input$ptrim_range[1], input$ptrim_range[2]), {
-  updateNumericInput(session, "ptrim_min", value = input$ptrim_range[1])
-  updateNumericInput(session, "ptrim_max", value = input$ptrim_range[2])
+
+# reactivate trim range number and limit in 1-5
+observeEvent(input$ml_plusButton, {
+  if(ml_trim_num() < 5) {ml_trim_num(ml_trim_num() + 1)}
+})
+observeEvent(input$ml_minusButton, {
+  if(ml_trim_num() > 1) {ml_trim_num(ml_trim_num() - 1)}
+})
+
+# convert ml_trim_range and ml_trim_min/max
+lapply(1:5, function(i) {
+  observeEvent(c(input[[paste0("ml_trim_min", i)]], input[[paste0("ml_trim_max", i)]]), {
+    updateSliderInput(session, paste0("ml_trim_range", i), value = c(input[[paste0("ml_trim_min", i)]], input[[paste0("ml_trim_max", i)]]))
+  })
+  observeEvent(c(input[[paste0("ml_trim_range", i)]][1], input[[paste0("ml_trim_range", i)]][2]), {
+    updateNumericInput(session, paste0("ml_trim_min", i), value = input[[paste0("ml_trim_range", i)]][1])
+    updateNumericInput(session, paste0("ml_trim_max", i), value = input[[paste0("ml_trim_range", i)]][2])
+  })
 })
 
 # prepare training datasets for scrs on click of button
@@ -28,10 +53,16 @@ observeEvent(input$prepare, {
       return()
     } else {
       hs_cur <- hs$val[[isolate(input$hs_selector_for_ml_prepare)]]
-      if (isolate(input$prepare_trim)) {
-        minR <- isolate(input$ptrim_range)[1]
-        maxR <- isolate(input$ptrim_range)[2]
-        hs_cur <- hs_cur[, , minR ~ maxR]
+      if (isolate(input$ml_trim)) {
+        text_range <- NULL
+        for (i in 1:isolate(ml_trim_num())) {
+          min_cur <- isolate(input[[paste0("ml_trim_range", i)]])[1]
+          max_cur <- isolate(input[[paste0("ml_trim_range", i)]])[2]
+          text_cur <- paste0(min_cur, "~", max_cur)
+          text_range <- ifelse(is.null(text_range), text_cur, paste(text_range, text_cur, sep=", "))
+        }
+        text_range <- paste0("c(", text_range, ")")
+        hs_cur <- hs_cur[, , eval(parse(text = text_range))]
       }
       # randomly split
       total <- nrow(hs_cur)
--- inst/shinyapp/ui/ml_prepare.R
+++ inst/shinyapp/ui/ml_prepare.R
@@ -8,26 +8,21 @@ tabItem(
       title = "Settings", status = "info", solidHeader = TRUE, collapsible = FALSE,
       column(
         12,
-        fluidRow(
-          column(1, checkboxInput("prepare_trim", "Trim", value = TRUE), class = "top25"),
-          column(2, numericInput("ptrim_min", "Min", min = 0, max = 4000, step = 1, value = 2000, width = "100%")),
-          column(7, sliderInput("ptrim_range", " Selecting Wavelength Ranges:",
-            min = 0, max = 4000, value = c(2000, 2350),
-            step = 1, dragRange = F, width = "100%"
-          )),
-          column(2, numericInput("ptrim_max", "Max", min = 0, max = 4000, step = 1, value = 2350, width = "100%"))
-        ),
+        uiOutput("ml_trim_multi"),
         fluidRow(
           column(3, uiOutput("hs_select_for_ml_prepare")),
           column(3, numericInput("train_pct", "Percent for training", 80, min = 50, max = 95, step = 1)),
-          column(4, withBusyIndicatorUI(actionButton("prepare", "Prepare datasets", class = "btn-success")), class = "top25")
+          column(1, checkboxInput("ml_trim", "Trim", value = TRUE), class = "top25"),
+          column(1, br(), actionButton("ml_plusButton", "", icon = icon("plus"))),
+          column(1, br(), actionButton("ml_minusButton", "", icon = icon("minus"))),
+          column(2, withBusyIndicatorUI(actionButton("prepare", "Prepare datasets", class = "btn-success")), class = "top25")
         )
       )
     ),
     box(
       title = "Notes", status = "info", solidHeader = TRUE, collapsible = FALSE,
       column(
-        12, p("1. Select the wavelength region to keep, only one region is supported."),
+        12, p("1. Select the wavelength region to keep, use \"+\" and \"-\" buttons to select multiple ranges."),
         p("2. The wavelength region can be broader than your spectra, but should not be too narrow."),
         p("3. Choose the target spectra collection from the dropdown selector."),
         p("4. Row of the spectra table (bottom left) can be selected to visual the spectrum (bottom right).")
